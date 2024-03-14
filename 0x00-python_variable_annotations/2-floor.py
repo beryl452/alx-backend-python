c3c47e13e8f7d49526b7dcec7841013cfe8aab7b
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 10:47:35 +0000

    End of 2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+"""Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """Determines the floor of a float
+
+    Args:
+        n (float): a number
+
+    Returns:
+        int: the floor of the float
+    """
+    return math.floor(n)

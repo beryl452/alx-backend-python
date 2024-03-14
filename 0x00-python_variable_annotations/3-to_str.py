72e686ceeceae9ddf906c649151ff758c00b6fc1
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 11:15:03 +0000

    Fix 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """Determines the string representation of the floata float
+
+    Args:
+        n (float): a number
+
+    Returns:
+        int: the string representation of the float
+    """
+    return str(n)

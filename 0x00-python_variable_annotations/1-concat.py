59e9ecc161995179b6feb25239283ab2f95225ac
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 10:05:04 +0000

    End of 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat str1 and str2
+
+    Args:
+        str1 (str): a string
+        str2 (str): a string
+
+    Returns:
+        str: the concatenated strings
+    """
+    return str1 + str2

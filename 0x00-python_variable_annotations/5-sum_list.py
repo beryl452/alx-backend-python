ff2f08397027ece3c780b0644737346795eda1cf
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 14:03:22 +0000

    End of 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
 
-def sum_list(input_list: list) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """
     Calculate the sum of all elements in the input list.
 
     Args:
-        input_list (list[float]): A list of floating-point numbers.
+        input_list (List[float]): A list of floating-point numbers.
 
     Returns:
         float: The sum of all elements in the input list.

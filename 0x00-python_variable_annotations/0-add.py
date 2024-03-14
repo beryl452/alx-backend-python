6eaf72b01b83be641cfd523c7558b057cb15f3a9
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 09:31:43 +0000

    End of 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+def add(a: float, b: float) -> float:
+    """Do sum of a and b
+
+    Args:
+        a (float): number
+        b (float): number
+
+    Returns:
+        Resuult of addition
+    """
+    return a + b
+

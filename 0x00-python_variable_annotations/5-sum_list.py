a48b0c3ecc79ec08688f45d6a5cad1985b88c8b6
beryl452 <berylhoe+alx@gmail.com>
2024-03-14 13:54:33 +0000

    Fix 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Complex types - list of floats"""
+
+def sum_list(input_list: list) -> float:
+    """
+    Calculate the sum of all elements in the input list.
+
+    Args:
+        input_list (list[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of all elements in the input list.
+    """
+    return sum(input_list)

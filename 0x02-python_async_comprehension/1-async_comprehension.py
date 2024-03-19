9476613ae40e65b1dabdc795c8d66848f542ebb0
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-19 11:15:42 +0000

    End of 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ import random
 from typing import List
 
 
-async_generator = __import__('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:

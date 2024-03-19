45a14e32b43acf6ee25040e3653854e4a032297e
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-19 08:54:34 +0000

    Fix 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Let's execute multiple coroutines at the same time with async"""
 import asyncio
-import heapq
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 

218a079bb702dd5289e12784b6ccc86b8eec394d
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-19 11:12:48 +0000

    Add async_generator function to 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,9 @@ import random
 from typing import List
 
 
+async_generator = __import__('0-async_generator.py').async_generator
+
+
 async def async_comprehension() -> List[float]:
     """
     This function performs an asynchronous comprehension

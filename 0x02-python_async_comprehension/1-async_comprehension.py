b3b5fe598bff3ecf72ba3e90b0a26cc5228ed4ea
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-19 10:51:24 +0000

    End of 1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1. Async Comprehensions"""
+import random
+from typing import List
+
+
+async def async_comprehension() -> List[float]:
+    """
+    This function performs an asynchronous comprehension
+    on the result of an async generator.
+
+    Returns:
+        A list of floats generated from the async generator.
+    """
+    return [i async for i in async_generator()]

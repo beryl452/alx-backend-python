e76e003d899dc062958c48de5a67e7db37c3e95e
HOUESSOU Béryl <beryl.houessou@gmail.com>
2024-03-19 10:20:27 +0000

    End of 0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""3. Tasks"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    Asynchronous generator that yields random floating-point
+    numbers between 0 and 10.
+
+    Yields:
+        float: A random floating-point number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
